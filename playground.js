fa6b8f4ecfe1a6a4bfa85fd7a5cb3671beec38b5
daniel-lundin <daniel.t.lundin@gmail.com>
2018-04-21 21:59:36 +0200

    update playground

--- playground.js
+++ playground.js
@@ -1,20 +1,51 @@
-import { init, mjukna } from './index.js';
+import { init, mjukna } from "./index.js";
+const lorem =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dictum nisl et ligula commodo sollicitudin. Cras volutpat consectetur lorem, et lobortis purus dictum sit amet. Maecenas eget eros id lacus egestas maximus.";
 
 function setup() {
-  const root = document.getElementById('root');
-  const adder = document.getElementById('adder');
-  const remover = document.getElementById('remover');
-  const square = document.getElementById('square');
+  const root = document.getElementById("root");
+  const addHeader = document.getElementById("add-header");
+  const removeHeader = document.getElementById("remove-header");
+  const addParagraph = document.getElementById("add-paragraph");
+  const removeParagraph = document.getElementById("remove-paragraph");
+  const addBall = document.getElementById("add-ball");
+  const removeBall = document.getElementById("remove-ball");
+  const square = document.getElementById("square");
+  const article = document.getElementById("article");
 
-  adder.addEventListener('click', () => {
-    const div = document.createElement('div');
-    div.className = 'box';
+  addBall.addEventListener("click", () => {
+    const div = document.createElement("div");
+    div.className = "box";
     root.prepend(div);
     mjukna(div);
   });
 
-  remover.addEventListener('click', () => {
-    const div = document.querySelector('.box');
+  addParagraph.addEventListener("click", () => {
+    const p = document.createElement("p");
+    p.innerText = lorem;
+    article.appendChild(p);
+    mjukna(p);
+  });
+
+  removeParagraph.addEventListener("click", () => {
+    const p = document.querySelector("p");
+    article.removeChild(p);
+  });
+
+  addHeader.addEventListener("click", () => {
+    const h2 = document.createElement("h2");
+    h2.innerText = "Header";
+    article.appendChild(h2);
+    mjukna(h2);
+  });
+
+  removeHeader.addEventListener("click", () => {
+    const h2 = document.querySelector("h2");
+    article.removeChild(h2);
+  });
+
+  removeBall.addEventListener("click", () => {
+    const div = document.querySelector(".box");
     root.removeChild(div);
   });
 
