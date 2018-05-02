ea33f9b9b9f2348919cb3d65fffa1c910056a397
daniel-lundin <daniel.t.lundin@gmail.com>
2018-05-02 22:32:26 +0200

    make dumdom inline-block aware

--- tests/features/basics.js
+++ tests/features/basics.js
@@ -5,48 +5,102 @@ const { mjukna } = require("../../index.js");
 const { rafUntilStill } = require("../helpers/utils");
 
 feature("basics", scenario => {
-  scenario("adding elements", ({ before, after, given, when, then, and }) => {
-    const scope = {};
+  scenario(
+    "adding block elements",
+    ({ before, after, given, when, then, and }) => {
+      const scope = {};
 
-    before(() => {
-      dumdom.init();
-    });
+      before(() => {
+        dumdom.init();
+      });
 
-    after(() => {
-      dumdom.reset();
-    });
+      after(() => {
+        dumdom.reset();
+      });
 
-    given("a mjukt element", () => {
-      const div = document.createElement("div");
-      document.appendChild(div);
-      scope.element = div;
-      scope.previousPosition = div.getBoundingClientRect();
-      mjukna(div);
-    });
+      given("a mjukt element", () => {
+        const div = document.createElement("div");
+        document.appendChild(div);
+        scope.element = div;
+        scope.previousPosition = div.getBoundingClientRect();
+        mjukna(div);
+      });
 
-    when("a h1 is added", () => {
-      scope.h1 = document.createElement("h1");
-      scope.h1.style.height = 50;
-      document.prepend(scope.h1);
-      document.triggerMutationObserver();
-    });
+      when("a h1 is added", () => {
+        scope.h1 = document.createElement("h1");
+        scope.h1.style.height = 50;
+        document.prepend(scope.h1);
+        document.triggerMutationObserver();
+      });
 
-    then("element should be in the same place", () => {
-      const position = scope.element.getBoundingClientRect();
-      assert.deepStrictEqual(scope.previousPosition, position);
-    });
+      then("element should be in the same place", () => {
+        const position = scope.element.getBoundingClientRect();
+        assert.deepStrictEqual(scope.previousPosition, position);
+      });
 
-    and("eventually move in to its new place", async () => {
-      await rafUntilStill(scope.element);
-      const newPosition = scope.element.getBoundingClientRect();
-      const expected = {
-        top: scope.h1.style.height,
-        bottom: scope.h1.style.height + scope.element.style.height
-      };
-      assert.deepStrictEqual(newPosition.top, expected.top);
-      assert.deepStrictEqual(newPosition.bottom, expected.bottom);
-    });
-  });
+      and("eventually move in to its new place", async () => {
+        await rafUntilStill(scope.element);
+        const newPosition = scope.element.getBoundingClientRect();
+        const expected = {
+          top: scope.h1.style.height,
+          bottom: scope.h1.style.height + scope.element.style.height
+        };
+        assert.deepStrictEqual(newPosition.top, expected.top);
+        assert.deepStrictEqual(newPosition.bottom, expected.bottom);
+      });
+    }
+  );
+
+  scenario(
+    "adding inline block elements",
+    ({ before, after, given, when, then, and }) => {
+      const scope = {};
+
+      before(() => {
+        dumdom.init();
+      });
+
+      after(() => {
+        dumdom.reset();
+      });
+
+      given("a mjukt element", () => {
+        const div = document.createElement("div");
+        div.style.display = "inline-block";
+        document.appendChild(div);
+        scope.element = div;
+        scope.previousPosition = div.getBoundingClientRect();
+        mjukna(div);
+      });
+
+      when("a h1 is added and an inline block is added", () => {
+        scope.inline = document.createElement("div");
+        scope.inline.style.display = "inline-block";
+        document.prepend(scope.inline);
+        scope.h1 = document.createElement("h1");
+        scope.h1.style.height = 50;
+        document.prepend(scope.h1);
+        document.triggerMutationObserver();
+      });
+
+      then("element should be in the same place", () => {
+        const position = scope.element.getBoundingClientRect();
+        assert.deepStrictEqual(scope.previousPosition, position);
+      });
+
+      and("eventually move in to its new place", async () => {
+        await rafUntilStill(scope.element);
+        const newPosition = scope.element.getBoundingClientRect();
+        const expected = {
+          top: scope.h1.style.height,
+          left: scope.inline.style.width,
+          bottom: scope.h1.style.height + scope.element.style.height
+        };
+        assert.deepStrictEqual(newPosition.top, expected.top);
+        assert.deepStrictEqual(newPosition.bottom, expected.bottom);
+      });
+    }
+  );
 
   scenario("removing elements", ({ before, after, given, when, then, and }) => {
     const scope = {};
--- tests/helpers/element.js
+++ tests/helpers/element.js
@@ -53,15 +53,54 @@ Element.prototype.getBoundingClientRect = function() {
   };
 };
 
+function isInline(element) {
+  return element && element.style.display === "inline-block";
+}
+
+Element.prototype._rowHeights = function() {
+  const elements = this._parent.children;
+  return elements.reduce(
+    (heights, element, index) => {
+      if (index === 0) {
+        return [[element.style.height, 1]];
+      }
+      if (isInline(elements[index - 1]) && isInline(element)) {
+        const [maxHeight, columnCount] = heights[heights.length - 1];
+        heights[heights.length - 1] = [
+          Math.max(maxHeight, element.style.height),
+          columnCount + 1
+        ];
+        return heights;
+      }
+      return heights.concat([[element.style.height, 1]]);
+    },
+    [[0, 0]]
+  );
+};
+
 Element.prototype._getTop = function() {
+  // I am the document
   if (!this._parent) return 0;
 
   const myIndex = this._parent.children.indexOf(this);
-  const siblingHeight = this._parent.children
-    .slice(0, myIndex)
-    .map(e => e.style.height)
-    .reduce((acc, curr) => acc + curr, 0);
-  return siblingHeight + this._parent._getTop();
+  const rowHeights = this._rowHeights();
+
+  let height = 0;
+  let elementIndex = 0;
+  for (let i = 0; i < rowHeights.length; ++i) {
+    const [rowHeight, columnCount] = rowHeights[i];
+    if (elementIndex + columnCount > myIndex) {
+      break;
+    }
+    height += rowHeight;
+    elementIndex += columnCount;
+  }
+  return height + this._parent._getTop();
+  // const siblingHeight = this._parent.children
+  //   .slice(0, myIndex)
+  //   .map(e => e.style.height)
+  //   .reduce((acc, curr) => acc + curr, 0);
+  // return siblingHeight + this._parent._getTop();
 };
 
 Element.prototype.createElement = function(type) {
