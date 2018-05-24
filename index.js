baef067380ddacc8f3d3c1cfe12bea4ef63e4440
daniel-lundin <daniel.t.lundin@gmail.com>
2018-05-24 22:38:59 +0200

    Updates

--- index.js
+++ index.js
@@ -1,64 +1,51 @@
 import tween from "https://unpkg.com/spring-array@1.2.4/src/index.js?module";
 let mjuka = [];
-let disconnector;
 let observer;
-let _root;
+
 const observeConfig = {
   childList: true,
   subtree: true,
   attributes: true
 };
-const tension = 0.1;
-const deceleration = 0.65;
+const DEFAULT_TENSION = 0.1;
+const DEFAULT_DECELERATION = 0.65;
 
-function unObserved(fn) {
-  disconnector();
-  fn();
-  observer.observe(_root, observeConfig);
-}
+let inProgress = [];
 
-export function mjukna(element, config = { scale: false }) {
-  if (mjuka.length === 0) {
-    disconnector = init();
-  }
-  const item = {
-    element,
-    config,
-    previousPosition: element.getBoundingClientRect(),
-    targetPosition: element.getBoundingClientRect(),
-    animationOffsets: {
-      x: 0,
-      y: 0,
-      widthDiff: 0,
-      heightDiff: 0
-    },
-    stop: () => {}
-  };
-  mjuka.push(item);
-  return () => {
-    mjuka = mjuka.filter(mjuk => mjuk !== item);
-    if (mjuka.length === 0) {
-      disconnector();
-    }
-  };
+export function mjukna(
+  elements,
+  {
+    scale = false,
+    tension = DEFAULT_TENSION,
+    deceleration = DEFAULT_DECELERATION,
+    staggerBy = 0
+  } = {}
+) {
+  init();
+  [].concat(elements).forEach(element => {
+    // Stop any running animations for element
+    inProgress = inProgress.filter(([e, staggerTimer, stopper]) => {
+      if (e === element) {
+        clearInterval(staggerTimer);
+        stopper();
+      }
+      return e !== element;
+    });
+
+    const item = {
+      element,
+      config: { scale, tension, deceleration, staggerBy },
+      previousPosition: element.getBoundingClientRect(),
+      targetPosition: element.getBoundingClientRect(),
+      stop: () => {}
+    };
+    mjuka.push(item);
+  });
 }
 
 function init(root = document) {
-  _root = root;
-  observer = new MutationObserver(mutations => {
-    const addedNodes = mutations.reduce(
-      (added, mutation) => added.concat(...mutation.addedNodes),
-      []
-    );
-
-    addedNodes.forEach(node => {
-      if (node.tagName === "IMG") {
-        node.addEventListener("load", () => {
-          updateElements();
-        });
-      }
-    });
-
+  observer = new MutationObserver(() => {
+    if (mjuka.length === 0) return;
     updateElements();
   });
 
@@ -66,44 +53,44 @@ function init(root = document) {
   return () => observer.disconnect();
 }
 
-function FLIPTranslate(mjuk, newPosition) {
-  const { previousPosition, element, animationOffsets } = mjuk;
-  // mjuk.targetPosition = newPosition;
-  const xCenterDiff = previousPosition.x - newPosition.x + animationOffsets.x;
-  const yCenterDiff = previousPosition.y - newPosition.y + animationOffsets.y;
+function FLIPTranslate(mjuk, newPosition, index) {
+  const {
+    previousPosition,
+    element,
+    config: { tension, deceleration, staggerBy }
+  } = mjuk;
+  const xCenterDiff = previousPosition.x - newPosition.x;
+  const yCenterDiff = previousPosition.y - newPosition.y;
 
-  unObserved(() => {
-    element.style.transform = `translate(${xCenterDiff}px, ${yCenterDiff}px)`;
-  });
+  element.style.transform = `translate(${xCenterDiff}px, ${yCenterDiff}px)`;
 
-  mjuk.stop();
-  const stopper = tween({
-    from: [xCenterDiff, yCenterDiff],
-    to: [0, 0],
-    update([x, y]) {
-      unObserved(() => {
+  const progress = [element, void 0, () => {}];
+  inProgress.push(progress);
+
+  const runner =
+    staggerBy === 0 ? fn => fn() : fn => setTimeout(fn, index * staggerBy);
+  progress[1] = runner(() => {
+    progress[2] = tween({
+      from: [xCenterDiff, yCenterDiff],
+      to: [0, 0],
+      update([x, y]) {
         element.style.transform = `translate(${x}px, ${y}px)`;
-        animationOffsets.x = newPosition.left + x;
-        animationOffsets.y = newPosition.top + y;
-      });
-    },
-    done() {
-      unObserved(() => {
+      },
+      done() {
         element.style.transform = "";
-      });
-      mjuk.previousPosition = element.getBoundingClientRect();
-      animationOffsets.x = 0;
-      animationOffsets.y = 0;
-    },
-    tension,
-    deceleration
+      },
+      tension,
+      deceleration
+    });
   });
-
-  mjuk.stop = stopper;
 }
 
-function FLIPScaleTranslate(mjuk, newPosition) {
-  const { previousPosition, element } = mjuk;
+function FLIPScaleTranslate(mjuk, newPosition, index) {
+  const {
+    previousPosition,
+    element,
+    config: { tension, deceleration, staggerBy }
+  } = mjuk;
   const xCenterDiff =
     previousPosition.x +
     previousPosition.width / 2 -
@@ -117,35 +104,31 @@ function FLIPScaleTranslate(mjuk, newPosition) {
   const xScaleCompensation = previousPosition.width / newPosition.width;
   const yScaleCompensation = previousPosition.height / newPosition.height;
 
-  unObserved(() => {
-    mjuk.element.style.transform = `translate(${xCenterDiff}px, ${yCenterDiff}px) scale(${xScaleCompensation}, ${yScaleCompensation})`;
-  });
+  mjuk.element.style.transform = `translate(${xCenterDiff}px, ${yCenterDiff}px) scale(${xScaleCompensation}, ${yScaleCompensation})`;
 
-  mjuk.stop();
-  const stopper = tween({
-    from: [xCenterDiff, yCenterDiff, xScaleCompensation, yScaleCompensation],
-    to: [0, 0, 1, 1],
-    update([x, y, scaleX, scaleY]) {
-      unObserved(() => {
+  const progress = [element, void 0, () => {}];
+  inProgress.push(progress);
+
+  const runner =
+    staggerBy === 0 ? fn => fn() : fn => setTimeout(fn, index * staggerBy);
+  progress[1] = runner(() => {
+    progress[2] = tween({
+      from: [xCenterDiff, yCenterDiff, xScaleCompensation, yScaleCompensation],
+      to: [0, 0, 1, 1],
+      update([x, y, scaleX, scaleY]) {
         element.style.transform = `translate(${x}px, ${y}px) scale(${scaleX}, ${scaleY})`;
-        mjuk.previousPosition = element.getBoundingClientRect();
-      });
-    },
-    done() {
-      unObserved(() => {
+      },
+      done() {
         element.style.transform = "";
-      });
-      mjuk.previousPosition = element.getBoundingClientRect();
-    },
-    tension,
-    deceleration
+      },
+      tension,
+      deceleration
+    });
   });
-  mjuk.stop = stopper;
 }
 
 function updateElements() {
-  mjuka.forEach(mjuk => {
-    // TODO: listen to attributes and skip if mutation target is element
+  mjuka.forEach((mjuk, index) => {
     if (
       positionsEqual(
         mjuk.element.getBoundingClientRect(),
@@ -155,16 +138,15 @@ function updateElements() {
       return;
     }
 
-    unObserved(() => {
-      mjuk.element.style.transform = "";
-    });
+    mjuk.element.style.transform = "";
     const newPosition = mjuk.element.getBoundingClientRect();
     if (mjuk.config.scale) {
-      FLIPScaleTranslate(mjuk, newPosition);
+      FLIPScaleTranslate(mjuk, newPosition, index);
     } else {
-      FLIPTranslate(mjuk, newPosition);
+      FLIPTranslate(mjuk, newPosition, index);
     }
   });
+  mjuka = [];
 }
 
 function positionsEqual(pos1, pos2) {
