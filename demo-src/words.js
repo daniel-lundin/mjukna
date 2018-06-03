db6823a9b566164411d29c1bc6107be0b1515658
daniel-lundin <daniel.t.lundin@gmail.com>
2018-06-03 22:29:05 +0200

    float animation

--- demo-src/words.js
+++ demo-src/words.js
@@ -4,7 +4,7 @@ const lorem =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dictum nisl et ligula commodo sollicitudin. Cras volutpat consectetur lorem, et lobortis purus dictum sit amet. Maecenas eget eros id lacus egestas maximus.";
 
 function makeItMjukna() {
-  const elements = document.querySelectorAll("span, article");
+  const elements = document.querySelectorAll("span, article, img");
   mjukna([...elements]);
 }
 function setupListeners() {
@@ -57,11 +57,26 @@ function setupListeners() {
     box.classList.remove("medium");
     box.classList.remove("small");
   });
+
+  document.getElementById("float-image").addEventListener("click", () => {
+    makeItMjukna();
+    const image = document.querySelector("img");
+    image.style.float = "left";
+  });
+  document.getElementById("unfloat-image").addEventListener("click", () => {
+    makeItMjukna();
+    const image = document.querySelector("img");
+    image.style.float = "initial";
+  });
 }
 function setup() {
   setupListeners();
   const root = document.getElementById("root");
 
+  const image = new Image();
+  image.src =
+    "https://images.pexels.com/photos/127028/pexels-photo-127028.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
+  root.appendChild(image);
   lorem.split(" ").forEach(word => {
     const span = document.createElement("span");
     span.innerText = word;
