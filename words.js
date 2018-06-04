917b29b35a709cd33b9f2c15b41a30cce891985a
daniel-lundin <daniel.t.lundin@gmail.com>
2018-06-04 22:57:40 +0200

    Updates

--- words.js
+++ words.js
@@ -4,70 +4,78 @@ const lorem =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dictum nisl et ligula commodo sollicitudin. Cras volutpat consectetur lorem, et lobortis purus dictum sit amet. Maecenas eget eros id lacus egestas maximus.";
 
 function makeItMjukna() {
-  const elements = document.querySelectorAll("span, article, img");
+  const elements = document.querySelectorAll("span, article, img, .card-title");
   mjukna([...elements]);
 }
 function setupListeners() {
-  document.getElementById("make-font-small").addEventListener("click", () => {
-    makeItMjukna();
-    const elements = [...document.querySelectorAll("span")];
-    elements.forEach(e => {
-      e.classList.remove("medium");
-      e.classList.remove("big");
-      e.classList.add("small");
-    });
-  });
-  document.getElementById("make-font-medium").addEventListener("click", () => {
-    makeItMjukna();
-    const elements = [...document.querySelectorAll("span")];
-    elements.forEach(e => {
-      e.classList.remove("small");
-      e.classList.remove("big");
-      e.classList.add("medium");
-    });
-  });
-  document.getElementById("make-font-big").addEventListener("click", () => {
-    makeItMjukna();
-    const elements = [...document.querySelectorAll("span")];
-    elements.forEach(e => {
-      e.classList.remove("small");
-      e.classList.remove("medium");
-      e.classList.add("big");
+  const fontsizeButtons = document.querySelectorAll(".font-size");
+  fontsizeButtons.forEach(element => {
+    element.addEventListener("click", function() {
+      makeItMjukna();
+      fontsizeButtons.forEach(e => e.classList.remove("active"));
+      this.classList.add("active");
+
+      const elements = [...document.querySelectorAll("span")];
+      elements.forEach(e => {
+        e.classList.remove("small");
+        e.classList.remove("medium");
+        e.classList.remove("large");
+        e.classList.add(this.dataset.value);
+      });
     });
   });
 
-  document.getElementById("make-box-small").addEventListener("click", () => {
-    makeItMjukna();
-    const box = document.querySelector("article");
-    box.classList.remove("medium");
-    box.classList.remove("big");
-    box.classList.add("small");
+  const boxButtons = document.querySelectorAll(".box-size");
+  boxButtons.forEach(element => {
+    element.addEventListener("click", function() {
+      makeItMjukna();
+      boxButtons.forEach(e => e.classList.remove("active"));
+      this.classList.add("active");
+      const box = document.querySelector("article");
+      box.classList.remove("medium");
+      box.classList.remove("big");
+      box.classList.remove("small");
+      box.classList.add(this.dataset.value);
+    });
   });
-  document.getElementById("make-box-medium").addEventListener("click", () => {
-    makeItMjukna();
-    const box = document.querySelector("article");
-    box.classList.add("medium");
-    box.classList.remove("big");
-    box.classList.remove("small");
-  });
-  document.getElementById("make-box-big").addEventListener("click", () => {
-    makeItMjukna();
-    const box = document.querySelector("article");
-    box.classList.add("big");
-    box.classList.remove("medium");
-    box.classList.remove("small");
-  });
-
-  document.getElementById("float-image").addEventListener("click", () => {
-    makeItMjukna();
-    const image = document.querySelector("img");
-    image.style.float = "left";
-  });
-  document.getElementById("unfloat-image").addEventListener("click", () => {
-    makeItMjukna();
-    const image = document.querySelector("img");
-    image.style.float = "initial";
-  });
+  function resetImageStyle() {
+    document.querySelectorAll(".image-style").forEach(e => {
+      e.classList.remove("active");
+    });
+  }
+  document
+    .getElementById("float-image-left")
+    .addEventListener("click", function() {
+      makeItMjukna();
+      resetImageStyle();
+      const image = document.querySelector("img");
+      image.style.float = "left";
+      image.style.width = "50%";
+      image.style.margin = "10px";
+      this.classList.add("active");
+    });
+  document
+    .getElementById("unfloat-image")
+    .addEventListener("click", function() {
+      makeItMjukna();
+      resetImageStyle();
+      const image = document.querySelector("img");
+      image.style.float = "initial";
+      image.style.width = "100%";
+      image.style.margin = "0";
+      this.classList.add("active");
+    });
+  document
+    .getElementById("float-image-right")
+    .addEventListener("click", function() {
+      makeItMjukna();
+      resetImageStyle();
+      const image = document.querySelector("img");
+      image.style.float = "right";
+      image.style.width = "50%";
+      image.style.margin = "10px";
+      this.classList.add("active");
+    });
 }
 function setup() {
   setupListeners();
