686a07836636d512b2ea57b0747e8f73e91360e1
daniel-lundin <daniel.t.lundin@gmail.com>
2018-06-11 22:19:19 +0200

    remove tests

--- tests/features/basics.js
+++ tests/features/basics.js
@@ -5,11 +5,11 @@ const { feature } = require("kuta/lib/bdd");
 
 const { setupNewPage } = require("../helpers/browser.js");
 
-feature("basics", scenario => {
+feature.only("basics", scenario => {
   let browser;
 
   scenario.before(async () => {
-    browser = await puppeteer.launch();
+    browser = await puppeteer.launch({ headless: false });
   });
 
   scenario.after(() => browser.close());
@@ -100,6 +100,61 @@ feature("basics", scenario => {
     then("elements should stay in place", () => {
       scope.initialPositions.forEach((position, index) => {
         assert.deepStrictEqual(position, scope.newPositions[index]);
+      });
+    });
+
+    and("eventually move into its new places", async () => {
+      await page.waitForFunction(() => {
+        const elements = Array.from(document.querySelectorAll("div"));
+        return elements.reduce(
+          (still, element) => still && elementStill(element),
+          true
+        );
+      });
+    });
+  });
+
+  scenario("removing elements", ({ before, given, when, then, and }) => {
+    const scope = {};
+    let page;
+
+    before(async () => {
+      page = await setupNewPage(browser);
+    });
+
+    given("three mjukt elements", async () => {
+      scope.initialPositions = await page.evaluate(() => {
+        for (let i = 0; i < 3; ++i) {
+          const div = document.createElement("div");
+          div.style.display = "inline-block";
+          div.style.width = "100px";
+          div.style.height = "100px";
+          div.style.background = "red";
+          document.body.appendChild(div);
+        }
+        const elements = Array.from(document.querySelectorAll("div"));
+        mjukna(elements);
+
+        return Promise.resolve(elements.map(dumpClientRect));
+      });
+    });
+
+    when("the first element is removed", async () => {
+      scope.newPositions = await page.evaluate(() => {
+        const elements = Array.from(document.querySelectorAll("div"));
+        document.body.removeChild(elements[0]);
+
+        return new Promise(resolve =>
+          requestAnimationFrame(() => {
+            resolve(elements.map(dumpClientRect));
+          })
+        );
+      });
+    });
+
+    then("elements should stay in place", () => {
+      scope.initialPositions.slice(1).forEach((position, index) => {
+        assert.deepStrictEqual(position, scope.newPositions.slice(1)[index]);
       });
     });
 
