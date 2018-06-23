e9446fea180af17166b2232cef61d8941198568d
Daniel Lundin <d-dog@d-dog.se>
2018-06-23 23:17:58 +0200

    updated numbers demo

--- demos/numbers.js
+++ demos/numbers.js
@@ -1,72 +1,48 @@
 /* global mjukna */
 
-const detailedNumber = document.querySelector(".detailed-number");
-const numbers = document.querySelector(".numbers");
+const texts = [
+  "1 (one, also called unit, unity, and (multiplicative) identity) is a number, numeral, and glyph. It represents a single entity, the unit of counting or measurement. For example, a line segment of unit length is a line segment of length 1. It is also the first of the infinite sequence of natural numbers, followed by 2. ",
+  'Two is the smallest prime number, and the only even prime number (for this reason it is sometimes called "the oddest prime"). The next prime is three. Two and three are the only two consecutive prime numbers. 2 is the first Sophie Germain prime, the first factorial prime, the first Lucas prime, the first Ramanujan prime, and the first Smarandache–Wellin prime.',
+  "Three is the largest number still written with as many lines as the number represents. (The Ancient Romans usually wrote 4 as IIII, but this was almost entirely replaced by the subtractive notation IV in the Middle Ages.) To this day 3 is written as three lines in Roman and Chinese numerals. ",
+  "Four is the smallest composite number, its proper divisors being 1 and 2. 4 is the smallest squared prime (p2) and the only even number in this form. 4 is also the only square one more than a prime number.  A number is a multiple of 4 if its last two digits are a multiple of 4. For example, 1092 is a multiple of 4 because 92 = 4 × 23."
+];
+function makeItMjukna() {
+  const cells = [...document.querySelectorAll(".cell")];
+  const headings = [...document.querySelectorAll(".cell span")];
 
-let selectedNumber;
-
-function unselectNumber(number) {
-  if (!number) return;
-
-  const h1 = document.querySelector("h1");
-  h1.parentNode.removeChild(h1);
-}
-
-function selectNumber(cell, number) {
-  mjukna(
-    [
-      ...document.querySelectorAll(".cell"),
-      ...document.querySelectorAll("h1"),
-      {
-        anchor: () => cell.querySelector("span"),
-        element: () => document.querySelector(".detailed-number h1 span")
-      }
-    ],
-    { exitFilter: () => true }
-  );
-  unselectNumber(selectedNumber);
-  const bigNumber = document.createElement("h1");
-  const span = document.createElement("span");
-  span.innerText = number;
-  bigNumber.appendChild(span);
-  detailedNumber.appendChild(bigNumber);
-  cell.parentNode.removeChild(cell);
-
-  selectedNumber = number;
-}
-
-function createCell(number) {
-  const cell = document.createElement("cell");
-  cell.classList.add("cell");
-  cell.setAttribute("id", `cell-${number}`);
-  const span = document.createElement("span");
-  span.innerText = number;
-  cell.appendChild(span);
-  cell.addEventListener("click", () => selectNumber(cell, number));
-  return cell;
-}
-
-const byId = id => document.getElementById(id);
-function setupListeners() {
-  byId("add-button").addEventListener("click", () => {
-    mjukna(
-      [
-        ...document.querySelectorAll(".cell"),
-        ...document.querySelectorAll(".cell span")
-      ],
-      {
-        enterFilter: () => true
-      }
-    );
-    const cell = createCell(numbers.children.length);
-    numbers.insertBefore(cell, byId("add-button"));
+  return mjukna([...cells, ...headings], {
+    enterFilter: () => true,
+    exitFilter: () => true
   });
 }
 
-function init() {
-  numbers.prepend(createCell(2));
-  numbers.prepend(createCell(1));
-  setupListeners();
+async function selectNumber(cell, index) {
+  const active = document.querySelector(".active");
+  if (active) {
+    const animation = makeItMjukna();
+    active.style.zIndex = 1;
+    active.classList.remove("active");
+    const info = active.querySelector("p");
+    info.parentNode.removeChild(info);
+    await animation;
+    active.style.zIndex = "";
+  }
+  if (cell === active) return;
+  const animation = makeItMjukna();
+  cell.classList.toggle("active");
+  await animation;
+
+  const info = document.createElement("p");
+  info.innerText = texts[index];
+  makeItMjukna();
+  cell.appendChild(info);
 }
 
-init();
+function setupListeners() {
+  const cells = Array.from(document.querySelectorAll(".cell"));
+  cells.forEach((cell, index) =>
+    cell.addEventListener("click", () => selectNumber(cell, index))
+  );
+}
+
+setupListeners();
