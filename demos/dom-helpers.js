e0012b37e9727dfc37e023ac98b92497e6cdf2e8
Daniel Lundin <d-dog@d-dog.se>
2018-07-07 23:07:03 +0200

    added guitar transition demo

---
+++ demos/dom-helpers.js
@@ -0,0 +1,30 @@
+function createElement(type, props, children) {
+  const element = document.createElement(type);
+
+  const attributes = Object.keys(props).filter(prop => !prop.startsWith("on"));
+  const eventHandlers = Object.keys(props).filter(prop =>
+    prop.startsWith("on")
+  );
+
+  attributes.forEach(prop => {
+    element.setAttribute(prop, props[prop]);
+  });
+  eventHandlers.forEach(handler => {
+    const eventName = handler.substr(2).toLowerCase();
+    element.addEventListener(eventName, props[handler]);
+  });
+
+  if (typeof children === "string") {
+    element.innerText = children;
+  } else {
+    [].concat(children).forEach(child => element.appendChild(child));
+  }
+
+  return element;
+}
+const tags = ["div", "img", "span", "h1", "h2", "h3"];
+const h = tags.reduce((acc, tag) => {
+  return Object.assign(acc, {
+    [tag]: (props, children = []) => createElement(tag, props, children)
+  });
+}, {});
---
+++ demos/guitars.js
@@ -0,0 +1,84 @@
+/* global mjukna, GUITARS, h */
+
+const grid = document.querySelector(".grid");
+
+function onDetailClose(index) {
+  const gridItem = document.querySelectorAll(".grid-item")[index];
+  const detailView = document.querySelector(".detail-view");
+  mjukna([
+    {
+      anchor: detailView.querySelector("h2 span"),
+      element: () => gridItem.querySelector("h2 span")
+    },
+    {
+      anchor: detailView.querySelector("img"),
+      element: () => gridItem.querySelector("img")
+    },
+    {
+      anchor: detailView.querySelector(".content"),
+      element: () => gridItem.querySelector(".background")
+    }
+  ]);
+
+  detailView.parentNode.removeChild(detailView);
+}
+
+function onProductClick(index) {
+  const guitar = GUITARS[index];
+  const gridItem = document.querySelectorAll(".grid-item")[index];
+  const detailView = h.div({ class: "detail-view" }, [
+    h.div({ class: "header" }, [
+      h.h2({}, h.span({}, guitar.title)),
+      h.div({ class: "close", onClick: () => onDetailClose(index) }, "X")
+    ]),
+    h.div({ class: "content" }, [
+      h.h3({}, guitar.subtitle),
+      h.div({ class: "price" }, `$${guitar.price}`),
+      h.div({ class: "description" }, guitar.description)
+    ]),
+    h.img({
+      src: guitar.img,
+      onLoad: async () => {
+        // Don't start animation until image is loaded
+        const animation = mjukna([
+          {
+            anchor: gridItem.querySelector("h2 span"),
+            element: () => detailView.querySelector("h2 span")
+          },
+          {
+            anchor: gridItem.querySelector("img"),
+            element: () => detailView.querySelector("img")
+          },
+          {
+            anchor: gridItem.querySelector(".background"),
+            element: () => detailView.querySelector(".content")
+          }
+        ]);
+        document.body.appendChild(detailView);
+        await animation;
+        detailView.classList.add("active");
+      }
+    })
+  ]);
+}
+
+function buildGrid() {
+  GUITARS.forEach((guitar, index) => {
+    const gridItem = h.div(
+      {
+        class: "grid-item",
+        onClick: () => onProductClick(index)
+      },
+      h.div({ class: "product" }, [
+        h.div({ class: "background" }),
+        h.img({ src: guitar.img }),
+        h.h2({}, h.span({}, guitar.title)),
+        h.h3({}, guitar.subtitle)
+      ])
+    );
+
+    grid.appendChild(gridItem);
+  });
+}
+
+buildGrid();
