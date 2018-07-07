44ffd8bc1a29ecf3c0c677272f42b0122c428b9e
Daniel Lundin <d-dog@d-dog.se>
2018-07-07 23:07:50 +0200

    Updates

---
+++ guitardata.js
@@ -0,0 +1,66 @@
+const GUITARS = [
+  {
+    title: "Marble Dream",
+    subtitle: "CONSTANTIN FRECKER",
+    price: "129",
+    img: "assets/guitars/1.png",
+    description:
+      "Hashtag cred air plant drinking vinegar. Leggings yuccie chambray pop-up tousled hell of. Portland wolf mumblecore, synth cold-pressed polaroid poke cardigan gochujang farm-to-table photo booth."
+  },
+  {
+    title: "Space Fantasy",
+    subtitle: "DANICA GREEN",
+    price: "199",
+    img: "assets/guitars/2.png",
+    description:
+      "Man bun banjo pop-up meh hammock. Skateboard hammock tousled retro, etsy taiyaki narwhal gentrify fixie food truck microdosing sustainable dreamcatcher."
+  },
+  {
+    title: "Mighty Eighties",
+    subtitle: "ELIZABETH SMITH",
+    price: "159",
+    img: "assets/guitars/3.png",
+    description:
+      "Air plant affogato microdosing banjo, palo santo squid craft beer vexillologist chambray everyday carry cronut aesthetic intelligentsia."
+  },
+  {
+    title: "Diamond Crafter",
+    subtitle: "FRED HOUSE",
+    price: "199",
+    img: "assets/guitars/4.png",
+    description:
+      "Crucifix shoreditch tumblr heirloom irony tbh gastropub migas sartorial mustache direct trade plaid readymade ramps hammock."
+  },
+  {
+    title: "Disco Fever",
+    subtitle: "ALICE MULLER",
+    img: "assets/guitars/5.png",
+    price: "99",
+    description:
+      "Single-origin coffee air plant kitsch paleo iPhone vegan cold-pressed slow-carb cornhole dreamcatcher palo santo salvia lo-fi."
+  },
+  {
+    title: "Little Boxes",
+    subtitle: "XAVIER BRIGHTON",
+    img: "assets/guitars/6.png",
+    price: "299",
+    description:
+      "Drinking vinegar lumbersexual 90's flexitarian. Live-edge man bun air plant XOXO. Master cleanse vaporware keffiyeh."
+  },
+  {
+    title: "Fractal Love",
+    subtitle: "WALTER PERRY",
+    img: "assets/guitars/7.png",
+    price: "129",
+    description:
+      "Leggings green juice DIY, flannel tattooed selvage plaid yr sartorial chia. Scenester you probably haven't heard of them locavore."
+  },
+  {
+    title: "Liquidia",
+    subtitle: "LIDIA GREENWOOD",
+    img: "assets/guitars/8.png",
+    price: "249",
+    description:
+      "Helvetica la croix readymade, butcher viral pitchfork chillwave pork belly vaporware blue bottle iceland semiotics subway tile irony."
+  }
+];
