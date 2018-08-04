e8f9059d94892d3f182a84e0ebf358270fbd746f
Daniel Lundin <d-dog@d-dog.se>
2018-08-04 23:19:09 +0200

    Updates

---
+++ anchor.js
@@ -0,0 +1,41 @@
+/* global mjukna, h */
+
+const grid = document.querySelector(".grid");
+
+async function dismiss(overlay, target) {
+  const animation = mjukna([
+    { anchor: overlay, element: () => target },
+    {
+      anchor: overlay.querySelector("span"),
+      element: () => target.querySelector("span")
+    }
+  ]);
+
+  grid.removeChild(overlay);
+  await animation;
+  target.classList.remove("active");
+}
+
+function open(target) {
+  const anchorNumber = target.querySelector("span");
+  const anchorColor = window.getComputedStyle(target).backgroundColor;
+  target.classList.add("active");
+
+  const overlay = h.div(
+    { class: "overlay", onClick: () => dismiss(overlay, target) },
+    h.span({}, anchorNumber.textContent)
+  );
+  overlay.style.background = anchorColor;
+
+  mjukna([
+    { anchor: target, element: () => overlay },
+    { anchor: anchorNumber, element: () => overlay.querySelector("span") }
+  ]);
+  grid.appendChild(overlay);
+}
+
+grid.addEventListener("click", function(event) {
+  if (event.target.classList.contains("box")) {
+    open(event.target);
+  }
+});
